f5c00e1702403e8ea550b278863a13ef95b5e664
LOTONGA Eliam <eliam.lotonga@ives.fr>
2018-07-25 15:44:06 +0200

    attributs casts

--- app/Event.php
+++ app/Event.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $guarded = ['id'];
-    protected $dates = ['starts_at'];
-
+    //protected $dates = ['starts_at']; // transforme les attributs passés dans le tableau en attribut de type Carbon ( DateTime custom)
+    protected $casts = [ // transforme les attributs (key) en types donnés (value)
+    	'starts_at' => 'dateTime'
+    ];
 
     public function isFree()
     {
